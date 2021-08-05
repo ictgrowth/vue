b4e55645f6f102037059532c0ae0abf2a4cea55a
Fadi Asharma <fadi.asharma@michaelhill.com.au>
2021-08-05 11:28:50 +1000

    Classes

--- app.js
+++ app.js
@@ -3,15 +3,38 @@ new Vue ({
     // Object
     data: {
         // String
-        name: 'Fadi',
-        job: 'Web developer',
+        name: '',
+        job: '',
         age: '27',
+        experience: '5',
         website: 'https://ictgrowth.com.au',
-        websiteTag: '<a href="https://ictgrowth.com.au">Personal Website</a>'
+        websiteTag: '<a href="https://ictgrowth.com.au">Personal Website</a>',
+        x: 0,
+        y: 0,
+        available: true,
     },
     methods: {
-        greeting: function(time)  {
-            return 'Good' + time + ' ' + this.name;
+        greeting: function()  {
+            return 'Hello,' + ' ' + this.name;
+        },
+        add: function(inc){
+            this.age += inc;
+            console.log('add')
+        },
+        subtract: function(dec){
+            this.age -= dec;
+            console.log('subtract')
+        },
+        updateXY: function(event){
+            // console.log(event);
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        click: function(){
+            alert("You clicked me");
         }
+    },
+    computed: {
+
     }
-})
+});
